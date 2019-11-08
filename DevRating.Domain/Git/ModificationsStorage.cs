50fd158a8ebf0f82293dfdd7f75074d2fd9a57b3
Viktor <viktor_semenov@outlook.com>
2019-11-08 23:41:10 +0900

    #33 Add todo

--- DevRating.Domain/Git/ModificationsStorage.cs
+++ DevRating.Domain/Git/ModificationsStorage.cs
@@ -4,6 +4,6 @@ namespace DevRating.Domain.Git
 {
     public interface ModificationsStorage
     {
-        string Insert(IEnumerable<Addition> additions, IEnumerable<Deletion> deletions);
+        string Insert(IEnumerable<Addition> additions, IEnumerable<Deletion> deletions); // TODO Return entities instead
     }
 }
