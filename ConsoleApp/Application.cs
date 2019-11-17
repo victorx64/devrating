f2826af5d4debdfab9463f3e7ebd304902a154f8
Viktor <viktor_semenov@outlook.com>
2019-11-18 01:47:55 +0900

    #35 Add Application

---
+++ ConsoleApp/Application.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Data;
+using DevRating.Domain;
+using DevRating.LibGit2SharpClient;
+using DevRating.SqlClient;
+using Microsoft.Data.SqlClient;
+
+namespace DevRating.ConsoleApp
+{
+    internal sealed class Application
+    {
+        private readonly Diff _diff;
+        private readonly IDbConnection _connection;
+        private readonly WorksRepository _works;
+
+        public Application(string path, string start, string end, string database)
+            : this(new LibGit2Diff(start, end, path), new TransactedDbConnection(new SqlConnection(database)))
+        {
+        }
+
+        public Application(Diff diff, IDbConnection connection)
+            : this(diff, connection, new SqlWorksRepository(connection))
+        {
+        }
+
+        public Application(Diff diff, IDbConnection connection, WorksRepository works)
+        {
+            _diff = diff;
+            _connection = connection;
+            _works = works;
+        }
+
+        public void PrintToConsole()
+        {
+            _connection.Open();
+            var transaction = _connection.BeginTransaction();
+
+            try
+            {
+                _diff.AddTo(_works);
+
+                PrintSavedToConsole();
+            }
+            finally
+            {
+                transaction.Rollback();
+                _connection.Close();
+            }
+        }
+
+        public void PrintSavedToConsole()
+        {
+            var work = _works.Work(_diff.Key());
+
+            Console.WriteLine("Reward:");
+            Console.WriteLine($"{work.Author()} {work.Reward():F2}");
+            Console.WriteLine("Rating updates:");
+
+            foreach (var rating in work.RatingUpdates())
+            {
+                Console.WriteLine($"{rating.Author()} {rating.NewRating():F2}");
+            }
+        }
+
+        public void Save()
+        {
+            _connection.Open();
+
+            var transaction = _connection.BeginTransaction();
+
+            try
+            {
+                _diff.AddTo(_works);
+
+                transaction.Commit();
+            }
+            catch
+            {
+                transaction.Rollback();
+
+                throw;
+            }
+            finally
+            {
+                _connection.Close();
+            }
+        }
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,106 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using DevRating.Domain;
-using DevRating.LibGit2SharpClient;
-using DevRating.SqlClient;
-using Microsoft.Data.SqlClient;
-
-namespace DevRating.ConsoleApp
+﻿namespace DevRating.ConsoleApp
 {
     internal static class Program
     {
-        private static async Task Main(string[] args)
+        private static void Main(string[] args)
         {
-            var repository = new LibGit2Repository(args[0]);
-            
-            var additions = new List<Addition>();
-            var deletions = new List<Deletion>();
-            
-            foreach (var commit in repository.Commits(args[1], args[2]))
+            var command = args[0];
+            var path = args[1];
+            var start = args[2];
+            var end = args[3];
+            var database =
+                @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DevRating;" +
+                @"Integrated Security=True;Persist Security Info=False;Pooling=False;" +
+                @"MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=False";
+
+            var application = new Application(path, start, end, database);
+
+            if (command.Equals("show"))
             {
-                await commit.WriteInto(additions, deletions);
+                application.PrintToConsole();
             }
 
-            var connection = new TransactedDbConnection(new SqlConnection(args[3]));
-
-            connection.Open();
-
-            var transaction = connection.BeginTransaction();
-
-            try
+            if (command.Equals("show-saved"))
             {
-                var storage = new SqlModificationsStorage(connection, new EloFormula());
-
-                storage.InsertAdditions(NonDeletedAdditions(additions, deletions));
-                storage.InsertDeletions(NonSelfDeletions(deletions));
-
-                Console.WriteLine("Rewards:");
-                
-                foreach (var commit in repository.Commits(args[1], args[2]))
-                {
-                    foreach (var reward in storage.RewardsOf(commit))
-                    {
-                        Console.WriteLine($"{reward.Author()} {reward.Value():F2} {reward.HasRating()}");
-                    }
-                }
-
-                Console.WriteLine("New Ratings:");
-                
-                foreach (var commit in repository.Commits(args[1], args[2]))
-                {
-                    foreach (var rating in storage.RatingsOf(commit))
-                    {
-                        Console.WriteLine($"{rating.Author()} {rating.Value():F2}");
-                    }
-                }
-
-                //transaction.Commit();
-            }
-            catch
-            {
-                transaction.Rollback();
-
-                throw;
-            }
-            finally
-            {
-                connection.Close();
-            }
-        }
-        
-        public static IEnumerable<Addition> NonDeletedAdditions(IEnumerable<Addition> additions, IEnumerable<Deletion> deletions)
-        {
-            foreach (var addition in additions)
-            {
-                yield return addition.NewAddition(
-                    addition.Count() -
-                    AddedThenDeletedLinesCount(addition.Commit(), deletions));
-            }
-        }
-
-        private static uint AddedThenDeletedLinesCount(Commit commit, IEnumerable<Deletion> deletions)
-        {
-            foreach (var deletion in deletions)
-            {
-                if (deletion.PreviousCommit().Equals(commit) &&
-                    deletion.PreviousCommit().AuthorEmail().Equals(deletion.Commit().AuthorEmail()))
-                {
-                    return deletion.Count();
-                }
+                application.PrintSavedToConsole();
             }
 
-            return 0;
-        }
-        
-        public static IEnumerable<Deletion> NonSelfDeletions(IEnumerable<Deletion> deletions)
-        {
-            foreach (var deletion in deletions)
+            if (command.Equals("save"))
             {
-                if (!deletion.Commit().AuthorEmail().Equals(deletion.PreviousCommit().AuthorEmail()))
-                {
-                    yield return deletion;
-                }
+                application.Save();
             }
         }
     }
