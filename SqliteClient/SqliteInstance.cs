491cef418f929dac824f8ca8e18b244ede038fa2
Viktor <viktor_semenov@outlook.com>
2019-11-21 00:51:33 +0900

    #35 Add SQLite scripts

--- SqliteClient/SqliteInstance.cs
+++ SqliteClient/SqliteInstance.cs
@@ -27,12 +27,58 @@ namespace DevRating.SqliteClient
 
         public void Create()
         {
-            throw new System.NotImplementedException();
+            using var command = _connection.CreateCommand();
+
+            command.CommandText = @"
+                CREATE TABLE [Author]
+                (
+                    [Id]    [int] IDENTITY(1, 1) NOT NULL,
+                    [Email] [nvarchar](50)       NOT NULL,
+                    CONSTRAINT [PK_Author] PRIMARY KEY ([Id] ASC),
+                    CONSTRAINT [UK_Author_Email] UNIQUE ([Email] ASC)
+                );
+                CREATE TABLE [Rating]
+                (
+                    [Id]               [int] IDENTITY(1, 1) NOT NULL,
+                    [Rating]           [real]               NOT NULL,
+                    [PreviousRatingId] [int]                NULL
+                        CONSTRAINT [FK_Rating_PreviousRatingId] REFERENCES [Rating] ([Id]),
+                    [WorkId]           [int]                NOT NULL
+                        CONSTRAINT [FK_Rating_WorkId] REFERENCES [Work] ([Id]),
+                    [AuthorId]         [int]                NOT NULL
+                        CONSTRAINT [FK_Rating_AuthorId] REFERENCES [Author] ([Id]),
+                    CONSTRAINT [PK_Rating] PRIMARY KEY ([Id] ASC)
+                );
+                CREATE TABLE [Work]
+                (
+                    [Id]           [int] IDENTITY(1, 1) NOT NULL,
+                    [Repository]   [nvarchar]           NOT NULL,
+                    [StartCommit]  [nvarchar](50)       NOT NULL,
+                    [EndCommit]    [nvarchar](50)       NOT NULL,
+                    [AuthorId]     [int]                NOT NULL
+                        CONSTRAINT [FK_Work_AuthorId] REFERENCES [Author] ([Id]),
+                    [Reward]       [real]               NOT NULL,
+                    [UsedRatingId] [int]                NULL
+                        CONSTRAINT [FK_Work_RatingId] REFERENCES [Rating] ([Id]),
+                    CONSTRAINT [PK_Work] PRIMARY KEY ([Id] ASC),
+                    CONSTRAINT [UK_Work_Commits] UNIQUE ([StartCommit] ASC, [EndCommit] ASC)
+                );
+                CREATE UNIQUE INDEX [UK_Rating_PreviousRatingId] ON [Rating] ([PreviousRatingId] ASC)
+                    WHERE ([PreviousRatingId] IS NOT NULL);";
+
+            command.ExecuteNonQuery();
         }
 
         public void Drop()
         {
-            throw new System.NotImplementedException();
+            using var command = _connection.CreateCommand();
+
+            command.CommandText = @"
+                DROP TABLE [Rating];
+                DROP TABLE [Author];
+                DROP TABLE [Work];";
+
+            command.ExecuteNonQuery();
         }
 
         public bool Exist()
