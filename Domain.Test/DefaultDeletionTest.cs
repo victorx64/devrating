a678ca1140ad02a2530407e31602b24b58d0974f
Viktor <viktor_semenov@outlook.com>
2020-01-14 14:49:24 +0900

    #94 Add DefaultDeletionTest

---
+++ Domain.Test/DefaultDeletionTest.cs
@@ -0,0 +1,23 @@
+using Xunit;
+
+namespace DevRating.Domain.Test
+{
+    public class DefaultDeletionTest
+    {
+        [Fact]
+        public void ReturnsEmailFromCtor()
+        {
+            var email = "some email";
+
+            Assert.Equal(new DefaultDeletion(email, 1).Email(), email);
+        }
+
+        [Fact]
+        public void ReturnsCountFromCtor()
+        {
+            var count = 2u;
+
+            Assert.Equal(new DefaultDeletion("some other email", count).Count(), count);
+        }
+    }
+}
