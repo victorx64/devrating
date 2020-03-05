c896b4ded516972605cf9c9da5f84f8ed902660d
Viktor <viktor_semenov@outlook.com>
2020-03-06 04:50:34 +0900

    #144 Add license information

--- LibGit2SharpClient/LibGit2Diff.cs
+++ LibGit2SharpClient/LibGit2Diff.cs
@@ -1,3 +1,6 @@
+// Copyright (c) 2019-present Viktor Semenov
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 using DevRating.Domain;
 using DevRating.VersionControl;
 using LibGit2Sharp;
