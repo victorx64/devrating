7951338bbdfedb9bc75b7b15aeefd27d30eac1fe
Victor Semenov <viktor_semenov@outlook.com>
2021-02-16 23:11:12 +0900

    #208 Fix option alias

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -22,7 +22,7 @@ namespace DevRating.ConsoleApp
             var addMergeCommand = new Command("commit", "Evaluate the reward for a merge commit and insert it into the local DB");
             addMergeCommand.AddOption(new Option<DirectoryInfo>(new[] { "--path", "-p" }, "Path to a local repository. E.g. '~/repos/devrating'") { IsRequired = true }.ExistingOnly());
             addMergeCommand.AddOption(new Option<string>(new[] { "--merge", "-m" }, "A merge commit. Takes diff of <merge>~ and <merge>") { IsRequired = true });
-            addMergeCommand.AddOption(new Option<string>(new[] { "--email", "-e" }, "Email of the PR author"));
+            addMergeCommand.AddOption(new Option<string>(new[] { "--email", "-a" }, "Email of the PR author"));
             addMergeCommand.AddOption(new Option<string>(new[] { "--link", "-l" }, "A link to the PR, issue or so"));
             addMergeCommand.AddOption(new Option<string>(new[] { "--org", "-o" }, "Name of the repository owner"));
             addMergeCommand.AddOption(new Option<string>(new[] { "--name", "-n" }, "Name of the repository"));
@@ -57,7 +57,7 @@ namespace DevRating.ConsoleApp
             addDiffCommand.AddOption(new Option<DirectoryInfo>(new[] { "--path", "-p" }, "Path to a local repository. E.g. '~/repos/devrating'") { IsRequired = true }.ExistingOnly());
             addDiffCommand.AddOption(new Option<string>(new[] { "--base", "-b" }, "The first commit of diff") { IsRequired = true });
             addDiffCommand.AddOption(new Option<string>(new[] { "--head", "-e" }, "The second commit of diff") { IsRequired = true });
-            addDiffCommand.AddOption(new Option<string>(new[] { "--email", "-e" }, "Email of the PR author"));
+            addDiffCommand.AddOption(new Option<string>(new[] { "--email", "-a" }, "Email of the PR author"));
             addDiffCommand.AddOption(new Option<string>(new[] { "--link", "-l" }, "A link to the PR, issue or so"));
             addDiffCommand.AddOption(new Option<string>(new[] { "--org", "-o" }, "Name of the repository owner"));
             addDiffCommand.AddOption(new Option<string>(new[] { "--name", "-n" }, "Name of the repository"));
@@ -90,7 +90,7 @@ namespace DevRating.ConsoleApp
             var serializeMergeCommand = new Command("commit", "Serialize merge commit metadata");
             serializeMergeCommand.AddOption(new Option<DirectoryInfo>(new[] { "--path", "-p" }, "Path to a local repository. E.g. '~/repos/devrating'") { IsRequired = true }.ExistingOnly());
             serializeMergeCommand.AddOption(new Option<string>(new[] { "--merge", "-m" }, "A merge commit. Takes diff of <merge>~ and <merge>") { IsRequired = true });
-            serializeMergeCommand.AddOption(new Option<string>(new[] { "--email", "-e" }, "Email of the PR author"));
+            serializeMergeCommand.AddOption(new Option<string>(new[] { "--email", "-a" }, "Email of the PR author"));
             serializeMergeCommand.AddOption(new Option<string>(new[] { "--link", "-l" }, "A link to the PR, issue or so"));
             serializeMergeCommand.AddOption(new Option<string>(new[] { "--org", "-o" }, "Name of the repository owner"));
             serializeMergeCommand.AddOption(new Option<string>(new[] { "--name", "-n" }, "Name of the repository"));
@@ -123,7 +123,7 @@ namespace DevRating.ConsoleApp
             serializeDiffCommand.AddOption(new Option<DirectoryInfo>(new[] { "--path", "-p" }, "Path to a local repository. E.g. '~/repos/devrating'") { IsRequired = true }.ExistingOnly());
             serializeDiffCommand.AddOption(new Option<string>(new[] { "--base", "-b" }, "The first commit of diff") { IsRequired = true });
             serializeDiffCommand.AddOption(new Option<string>(new[] { "--head", "-e" }, "The second commit of diff") { IsRequired = true });
-            serializeDiffCommand.AddOption(new Option<string>(new[] { "--email", "-e" }, "Email of the PR author"));
+            serializeDiffCommand.AddOption(new Option<string>(new[] { "--email", "-a" }, "Email of the PR author"));
             serializeDiffCommand.AddOption(new Option<string>(new[] { "--link", "-l" }, "A link to the PR, issue or so"));
             serializeDiffCommand.AddOption(new Option<string>(new[] { "--org", "-o" }, "Name of the repository owner"));
             serializeDiffCommand.AddOption(new Option<string>(new[] { "--name", "-n" }, "Name of the repository"));
