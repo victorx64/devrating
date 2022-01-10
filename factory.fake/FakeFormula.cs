b54c5960a8b22c1f79e0a769f625f3ba67619718
Victor Semenov <viktor_semenov@outlook.com>
2022-01-11 02:52:39 +0900

    #215 Fix test

--- factory.fake/FakeFormula.cs
+++ factory.fake/FakeFormula.cs
@@ -4,15 +4,22 @@ public sealed class FakeFormula : Formula
 {
     private readonly double _default;
     private readonly double _increase;
+    private readonly double _n;
 
-    public FakeFormula() : this(10d, 1d)
+    public FakeFormula() : this(10d, 1d, 400)
     {
     }
 
     public FakeFormula(double @default, double increase)
+        : this(@default, increase, 400)
+    {
+    }
+
+    public FakeFormula(double @default, double increase, double n)
     {
         _default = @default;
         _increase = increase;
+        _n = n;
     }
 
     public double DefaultRating()
@@ -22,11 +29,22 @@ public sealed class FakeFormula : Formula
 
     public double SuggestedAdditionsCount(double rating)
     {
-        throw new NotImplementedException();
+        return 1d - WinProbabilityOfA(rating, _default);
     }
 
     public double WinnerRatingChange(double winnerOldRating, double loserOldRating)
     {
         return _increase;
+    }
+
+    private double WinProbabilityOfA(double a, double b)
+    {
+        var qa = Math.Pow(10d, a / _n);
+
+        var qb = Math.Pow(10d, b / _n);
+
+        var ea = qa / (qa + qb);
+
+        return ea;
     }
 }
