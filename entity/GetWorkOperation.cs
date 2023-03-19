70e203e163b727fc63d4ca327c3fe340267194fc
Viсtor Semenov <viktor_semenov@outlook.com>
2023-03-19 20:26:21 +0700

    #242 Get last works of an author (#243)
    * #242 Get last works of an author
    
    * #242 Author last works

--- entity/GetWorkOperation.cs
+++ entity/GetWorkOperation.cs
@@ -5,4 +5,5 @@ public interface GetWorkOperation
     Work Work(string organization, string repository, string commit);
     Work Work(Id id);
     IEnumerable<Work> Last(string organization, string repository, DateTimeOffset after);
+    IEnumerable<Work> Last(Id author, DateTimeOffset after);
 }
